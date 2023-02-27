2462a5889f7aa2e4bbe083ea5bbb44ce997a8d58
Kender ROMAIN <kenderromain@gmail.com>
2023-02-26 23:46:07 -0400

    Create calculateTotals reducer.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './app/features/cartSlice';
+
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
