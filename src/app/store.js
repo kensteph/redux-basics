52a2408fc0a0a9f3fb814f81d1a624273fa663ee
Kender ROMAIN <kenderromain@gmail.com>
2023-02-26 22:20:48 -0400

    Create the Navbar component.

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import cartReducer from './features/cartSlice';
 // Configure the store
-const store = configureStore({ reducer: {} });
+const store = configureStore({ reducer: {cart:cartReducer} });
 
 // Export the store
 export default store;
